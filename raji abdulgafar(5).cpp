8c47002b180ec5d642c6257b7bd2dbaf466c4b7a
Ushie-Sylvester <33056522+Ushie-Sylvester@users.noreply.github.com>
2018-03-12 19:50:13 +0100

    Update raji abdulgafar(5).cpp

--- raji abdulgafar(5).cpp
+++ raji abdulgafar(5).cpp
@@ -8,7 +8,7 @@ DEPARTMENT: CYBERSECURITY SCIENCE
 using namespace std;
 void myFunc(){
 	// declaring the variables
-	double a,b,c,solve, root1,root2;
+	double a,b,c,d,solve, root1,root2,real,imaginary;
 	cout<< "Given a Quadratic equation determine the two root: take a as x^2, b as x and c as the constant respectively." <<endl;
 	cout<< "Enter the value of a: ";
 	// taking input from user
@@ -20,13 +20,29 @@ void myFunc(){
 	// taking input from user
 	cin>> c;
 	// computing your values
-	solve = sqrt(b*b - 4*a*c);
-	root1  = (-b - solve )/(2*a);
-	root2  = (-b + solve )/(2*a);
-	// printing result to screen
-	cout<< "The first root is " << root1 ; 
-	cout << endl;
-	cout<< "The second root is " << root2 ;
+	d=b*b-4*a*c;
+	if (d>0)
+	{
+		root1 = (-b + sqrt(d)) / (2*a);
+		root2 = (-b - sqrt(d)) / (2*a);
+		cout << "Roots are: " << endl;
+		cout << "x1 = " << root1 << endl;
+		cout << "x2 = " << root2 << endl;
+	
+	}
+	else if (d == 0) 
+	{
+		cout << "Roots are: " << endl;
+		root1 = (-b + sqrt(d)) / (2*a);
+		cout << "x1 = x2 =" << root1 << endl;
+	}
+	else { //this is used when d value of d is a negative number so it is not considered
+		real = -b/(2*a);
+		imaginary =sqrt(-d)/(2*a);//the imaginary number i.e negative number
+		cout << "Roots are: " << endl;
+		cout << "x1 = " << real << "+" << imaginary << "i" << endl;//values showing with "i" at the end are imaginary the are not part of the answer
+		cout << "x2 = " << real << "-" << imaginary << "i" << endl;
+		}
 }
 int main() {
 
